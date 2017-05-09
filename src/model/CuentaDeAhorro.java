5831420edade7279a3e5701446426a629c6b755d
Jorge Luis Chuc Lopez <jorge.chuc@itcampeche.edu.mx>
2017-05-09 11:00:41 -0500

    Se estableció una relación de herencia entre CuentaDeAhorros y CuentaBasica

--- src/model/CuentaDeAhorro.java
+++ src/model/CuentaDeAhorro.java
@@ -9,62 +9,21 @@ package model;
  *
  * @author JorgeLuis
  */
-public class CuentaDeAhorro {
+public class CuentaDeAhorro extends CuentaBasica {
     private final double TASA_DE_INTERES_PREDET = 4.0;
     private final double COMISION_POR_SALDO_PREDET = 50.0;
-    private int numeroDeCuenta;
-    private String cliente;
-    private double saldo;
     private double tasaDeInteres;
     private double comisionPorSaldo;
     
     public CuentaDeAhorro(int numeroDeCuenta, String cliente) {
-        this.numeroDeCuenta = numeroDeCuenta;
-        this.cliente = cliente;
-        this.saldo = 0.0;
+        super(numeroDeCuenta, cliente);
         this.tasaDeInteres = TASA_DE_INTERES_PREDET;
         comisionPorSaldo = COMISION_POR_SALDO_PREDET;
     }
     
-    public double getSaldo() {
-        return this.saldo;
-    }
-    
-    public boolean depositar(double cantidad) {
-        boolean seRealizoDeposito;
-        if (cantidad > 0.0) {
-            saldo = saldo + cantidad;
-            seRealizoDeposito = true;
-        }
-        else {
-            seRealizoDeposito = false;
-        }
-        return seRealizoDeposito;
-    }
-
-    public boolean retirar(double cantidad) {
-        boolean seRealizoRetiro;
-        if (cantidad <= this.getSaldo()) {
-            saldo = this.saldo - cantidad;
-            seRealizoRetiro = true;
-        }
-        else {
-            seRealizoRetiro = false;
-        }
-        return seRealizoRetiro;
-    }
-
-    public int getNumeroDeCuenta() {
-        return this.numeroDeCuenta;
-    }
-
-    public String getCliente() {
-        return this.cliente;
-    }
-    
     public double calcularInteres() {
         double tasaDeInteresDiaria = this.tasaDeInteres / 360;
-        double interes = tasaDeInteresDiaria / 100.0 * 30 * this.saldo;
+        double interes = tasaDeInteresDiaria / 100.0 * 30 * this.getSaldo();
         return interes;
     }
     
